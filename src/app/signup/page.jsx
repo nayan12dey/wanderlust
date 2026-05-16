6475a933bf519d2e06f5a307ee7381d209efd6e9
Nayan Dey <ndeynayan123@gmail.com>
2026-05-16 20:08:56 +0530

    added signup page and functionalities

---
+++ src/app/signup/page.jsx
@@ -0,0 +1,118 @@
+
+"use client";
+import { authClient } from "@/lib/auth-client";
+import {Check} from "@gravity-ui/icons";
+import {Button, Card, Description, FieldError, Form, Input, Label, TextField} from "@heroui/react";
+import { redirect } from "next/navigation";
+
+
+const SignUpPage = () => {
+
+    const onSubmit = async(e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget)
+        const user = Object.fromEntries(formData.entries())
+        
+        console.log(user);
+
+        const {data, error} = await authClient.signUp.email({
+            email: user.email,
+            password: user.password,
+            name: user.name,
+            image: user.image
+        })
+
+        // console.log({data, error})
+
+        if(data){
+            redirect("/")
+        }
+        if(error){
+            // toast
+            alert(error?.message);
+        }
+
+    }
+
+
+
+    return (
+        <div className="max-w-7xl mx-auto">
+            <div className="text-center my-4">
+                <h1 className="text-2xl font-bold">Create Account</h1>
+                <p>Start your adventure with wanderlust</p>
+            </div>
+            <Card className="border p-10">
+                <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+                    <TextField
+                        isRequired
+                        name="name"
+                        type="text"
+                    >
+                        <Label>Name</Label>
+                        <Input placeholder="Enter Your Name" />
+                        <FieldError />
+                    </TextField>
+
+                    <TextField
+                        isRequired
+                        name="image"
+                        type="url"
+                    >
+                        <Label>Image URL</Label>
+                        <Input placeholder="Enter Image url" />
+                        <FieldError />
+                    </TextField>
+
+                    <TextField
+                        isRequired
+                        name="email"
+                        type="email"
+                        validate={(value) => {
+                            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                                return "Please enter a valid email address";
+                            }
+                            return null;
+                        }}
+                    >
+                        <Label>Email</Label>
+                        <Input placeholder="john@example.com" />
+                        <FieldError />
+                    </TextField>
+                    <TextField
+                        isRequired
+                        minLength={8}
+                        name="password"
+                        type="password"
+                        validate={(value) => {
+                            if (value.length < 8) {
+                                return "Password must be at least 8 characters";
+                            }
+                            if (!/[A-Z]/.test(value)) {
+                                return "Password must contain at least one uppercase letter";
+                            }
+                            if (!/[0-9]/.test(value)) {
+                                return "Password must contain at least one number";
+                            }
+                            return null;
+                        }}
+                    >
+                        <Label>Password</Label>
+                        <Input placeholder="Enter your password" />
+                        <Description>Must be at least 8 characters with 1 uppercase and 1 number</Description>
+                        <FieldError />
+                    </TextField>
+                    <div className="flex gap-2">
+                        <Button type="submit" className={"w-full bg-cyan-500"}>
+                            Create Account
+                        </Button>
+                       
+                    </div>
+                </Form>
+            </Card>
+        </div>
+    );
+};
+
+export default SignUpPage;
