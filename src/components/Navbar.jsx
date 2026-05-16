a09164456ba15760f6d73dc20849653675c7f88f
Nayan Dey <ndeynayan123@gmail.com>
2026-05-16 22:02:02 +0530

    added login page and showing user data on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
+"use client"
+
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Button } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
 
 
 
 const Navbar = () => {
+
+    const {
+        data: session,
+    } = authClient.useSession()
+
+    const user = session?.user
+    console.log(user);
+
     return (
-        <nav className="flex justify-between bg-white p-5">
+        <nav className="flex items-center justify-between bg-white p-5">
             <ul className="flex gap-3">
                 <li><Link href={"/"}>Home</Link></li>
                 <li><Link href={"/destinations"}>Destinations</Link></li>
@@ -17,10 +29,25 @@ const Navbar = () => {
                 <Image src={"/assets/wanderlust.png"} width={150} height={150} alt="logo"></Image>
             </div>
 
-            <ul className="flex gap-3">
+            <ul className="flex items-center gap-3">
                 <li><Link href={"/profile"}>Profile</Link></li>
-                <li><Link href={"/login"}>Login</Link></li>
-                <li><Link href={"/signup"}>Sign Up</Link></li>
+
+                {user ? <>
+                    <li>
+                        <Avatar>
+                            <Avatar.Image alt={user.name} src={user?.image} />
+                            <Avatar.Fallback>{user.name.charAt(0)}</Avatar.Fallback>
+                        </Avatar>
+                    </li>
+
+                    <li>
+                        <Button variant="danger" className={"rounded-none"}>Logout</Button>
+                    </li>
+
+                </> : <>
+                    <li><Link href={"/login"}>Login</Link></li>
+                    <li><Link href={"/signup"}>Sign Up</Link></li>
+                </>}
             </ul>
 
         </nav>
